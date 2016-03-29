190228cda3605b84a4b97d4e759b2daa089a3ada
Nathan Reed <nmr7393@rit.edu>
2016-03-29 01:44:17 -0400

    Added a route with a random quote

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 import os
+import random
 from flask import Flask
+
+from quotes import quotes
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/hello')
 def hello():
-	return "Hello world!"
+	return 'Hello world!'
+
+@app.route('/'):
+def trash():
+	return(random.choose(quotes))
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
