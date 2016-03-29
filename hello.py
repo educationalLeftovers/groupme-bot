6378232fa04b9a3faaa60301fbd7d6fab6ae18d5
Nathan Reed <nmr7393@rit.edu>
2016-03-29 03:20:37 -0400

    added requests wrote the 'trashman' groupme callback

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import os
 import random
-from flask import Flask
+from flask import Flask, json, request
+import requests
+
 
 from quotes import quotes
 
@@ -17,6 +19,22 @@ def trash():
 @app.route('/test')
 def test():
 	return(os.environ.get("CONFIG_ITEM", '(failed to get config item)'))
+
+@app.route('/callback/', methods=['POST'])
+def groupme_callback():
+	json_body = request.get_json()
+	if json_body['group_id'] == os.environ['GROUP_ID']:
+		# some degree of verification that it is sent via a groupme callback
+		# could also check for "User-Agent: GroupMeBotNotifier/1.0", but that's plenty spoofable
+
+		if 'trash' in json_body['text']:
+			payload = {
+				'bot_id' : os.environ['BOT_ID'],
+				'text'   : random.choice(quotes),
+			}
+			requests.post('https://api.groupme.com/v3/bots/post', json=payload)
+			return('ok, trash\n')
+		return('ok, no trash\n')
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
